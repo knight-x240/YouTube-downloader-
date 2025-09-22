2ecebe1a9581b79ff58d02d91844a4f4f802848a
knight-x240 <149903898+knight-x240@users.noreply.github.com>
2025-09-23 04:48:13 +0600

    Add files via upload

---
+++ config.py
@@ -0,0 +1,4 @@
+# Set your telegram bot token here
+BOT_TOKEN = "YOUR_TELEGRAM_BOT_TOKEN_HERE"
+# Telegram bot file upload limit in bytes (50MB for most bots)
+TELEGRAM_SIZE_LIMIT = 50 * 1024 * 1024
